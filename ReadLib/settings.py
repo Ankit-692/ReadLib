b393785c89135fa22bd895ec1b246a7fc754674c
Ankit-692 <bhartiankit692@gmail.com>
2022-09-09 19:20:14 +0530

    trying

--- ReadLib/settings.py
+++ ReadLib/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Secret_key',0)
+# SECRET_KEY = os.environ.get('Secret_key',0)
+SECRET_KEY = 'django-insecure-)78-l-sa2q*y92ti44^=+(hxw7xhou(6^m1g054u##ua^3w(2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
