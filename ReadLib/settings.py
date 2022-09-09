75813c67eddef20743f289afe2281e679f37b241
Ankit-692 <bhartiankit692@gmail.com>
2022-09-09 19:37:57 +0530

    hope to work finally

--- ReadLib/settings.py
+++ ReadLib/settings.py
@@ -15,6 +15,8 @@ import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('Secret_key',0)
+
 SECRET_KEY = 'django-insecure-)78-l-sa2q*y92ti44^=+(hxw7xhou(6^m1g054u##ua^3w(2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
